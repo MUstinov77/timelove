633cb92de97aba9a72db0588c9ad00ef72467480
Max Ustinov <m.ustinov2001@yandex.ru>
2026-05-09 11:50:55 +0300

    init new schema

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,108 @@
 package main
 
 import (
-	"timelove/storage"
+	"context"
+	"fmt"
+	"log"
+	"time"
+	"timelove/ent"
+	"timelove/ent/user"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	storage.CreateDb()
+	client, err := ent.Open("sqlite3", "file::memory:?mode=memory&cache=shared")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(25).
+		SetName("Max").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// `Only` fails if no user found,
+		// or more than 1 user returned.
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
+}
+
+func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	// Create a new car with model "Tesla".
+	tesla, err := client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", tesla)
+
+	// Create a new car with model "Ford".
+	ford, err := client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", ford)
+
+	// Create a new user, and add it the 2 cars.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		AddCars(tesla, ford).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", a8m)
+	return a8m, nil
+}
+
+func QueryCars(ctx context.Context, usr *ent.User) error {
+	cars, err := usr.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+
+	log.Println("returned cars:", cars)
+
+	ford, err := usr.QueryCars().
+		Where(car.Model("Ford")).
+		Only(ctx)
+
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+
+	log.Println(ford)
+	
+	return nil
 }
