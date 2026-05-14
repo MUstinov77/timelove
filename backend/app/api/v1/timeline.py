f9bda4decc0673474a1f81909d4f73cd29034944
MUstinov77 <m.ustinov2001@yandex.ru>
2026-05-14 09:33:27 +0300

    add notfound exc to timeline router

--- backend/app/api/v1/timeline.py
+++ backend/app/api/v1/timeline.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter, Depends
 from backend.app.schema.timeline import TimelineCreateUpdateSchema, TimelineResponseSchema
 from backend.app.service.timeline import TimelineService, get_timeline_service
 from backend.app.model.timeline import Timeline
+from backend.app.model.user import User
+from backend.app.core.exceptions import NotFoundException
 
 router = APIRouter(
     prefix="/timeline",
@@ -20,19 +22,20 @@ async def get_timeline(
 ):
     timeline = await timeline_service.retrieve_one(Timeline.id, timeline_id)
     if not timeline:
-        raise ...
+        raise NotFoundException
     return timeline
 
 @router.get(
-    "/",
+    "/{user_id}",
     response_model=list[TimelineResponseSchema]
 )
 async def get_my_timelines(
+    user_id: int,
     timeline_service: TimelineService = Depends(get_timeline_service)
 ):
-    timelines = await timeline_service.retrieve_all()
+    timelines = await timeline_service.retrieve_all(User.id, user_id)
     if not timelines:
-        raise ...
+        raise NotFoundException
     return timelines
 
 @router.post(
@@ -45,7 +48,7 @@ async def create_timeline(
 ):
     timeline = await timeline_service.create_instance(create_data.model_dump())
     if not timeline:
-        raise ...
+        raise NotFoundException
     return timeline
 
 @router.patch(
@@ -59,7 +62,7 @@ async def update_timeline(
 ):
     timeline = await timeline_service.update_instance(update_data.model_dump(), timeline_id)
     if not timeline:
-        raise ...
+        raise NotFoundException
     return timeline
 
 @router.delete("/{timeline_id}")
@@ -69,5 +72,5 @@ async def delete_timeline(
 ):
     timeline = await timeline_service.delete_instance(timeline_id)
     if not timeline:
-        raise ...
+        raise NotFoundException
     return timeline
