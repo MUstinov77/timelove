df6a79e9bd1c75e979c7e3f02521b46ef1271de1
MUstinov77 <m.ustinov2001@yandex.ru>
2026-05-14 09:28:19 +0300

    add timeline service to router

--- backend/app/api/v1/timeline.py
+++ backend/app/api/v1/timeline.py
@@ -2,8 +2,9 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-
 from backend.app.schema.timeline import TimelineCreateUpdateSchema, TimelineResponseSchema
+from backend.app.service.timeline import TimelineService, get_timeline_service
+from backend.app.model.timeline import Timeline
 
 router = APIRouter(
     prefix="/timeline",
@@ -14,10 +15,12 @@ router = APIRouter(
     response_model=TimelineResponseSchema
 )
 async def get_timeline(
-        timeline_id: int,
-        timeline_service: Annotated[TimelineService, Depends(get_timeline_service)]
+    timeline_id: int,
+    timeline_service: TimelineService = Depends(get_timeline_service)
 ):
-    timeline = ...
+    timeline = await timeline_service.retrieve_one(Timeline.id, timeline_id)
+    if not timeline:
+        raise ...
     return timeline
 
 @router.get(
@@ -25,9 +28,11 @@ async def get_timeline(
     response_model=list[TimelineResponseSchema]
 )
 async def get_my_timelines(
-    timeline_service: Annotated[TimelineService, Depends(get_timeline_service)]
+    timeline_service: TimelineService = Depends(get_timeline_service)
 ):
-    timelines = ...
+    timelines = await timeline_service.retrieve_all()
+    if not timelines:
+        raise ...
     return timelines
 
 @router.post(
@@ -35,10 +40,12 @@ async def get_my_timelines(
     response_model=TimelineResponseSchema,
 )
 async def create_timeline(
-        create_data: TimelineCreateSchema,
-        timeline_service: Annotated[TimelineService, Depends(get_timeline_service)]
+    create_data: TimelineCreateUpdateSchema,
+    timeline_service: Annotated[TimelineService, Depends(get_timeline_service)]
 ):
-    timeline = ...
+    timeline = await timeline_service.create_instance(create_data.model_dump())
+    if not timeline:
+        raise ...
     return timeline
 
 @router.patch(
@@ -46,16 +53,21 @@ async def create_timeline(
     response_model=TimelineResponseSchema
 )
 async def update_timeline(
-        timeline_id: int,
-        timeline_service: Annotated[TimelineService, Depends(get_timeline_service)]
+    timeline_id: int,
+    update_data: TimelineCreateUpdateSchema,
+    timeline_service: TimelineService = Depends(get_timeline_service)
 ):
-    timeline = ...
+    timeline = await timeline_service.update_instance(update_data.model_dump(), timeline_id)
+    if not timeline:
+        raise ...
     return timeline
 
 @router.delete("/{timeline_id}")
 async def delete_timeline(
-        timeline_id: int,
-        timeline_service: Annotated[TimelineService, Depends(get_timeline_service)]
+    timeline_id: int,
+    timeline_service: TimelineService = Depends(get_timeline_service)
 ):
-    timeline = ...
+    timeline = await timeline_service.delete_instance(timeline_id)
+    if not timeline:
+        raise ...
     return timeline
