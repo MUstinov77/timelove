b56d0cea77f5f08a956bfa98ae17d42c36d70f7c
MUstinov77 <m.ustinov2001@yandex.ru>
2026-05-14 09:12:48 +0300

    create timeline service

---
+++ backend/app/service/timeline.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.app.core.datastore import postgres_session_provider
+from backend.app.model.timeline import Timeline
+from .base import BaseService
+
+
+def get_timeline_service(
+        session: AsyncSession = Depends(postgres_session_provider)
+):
+    return TimelineService(session, Timeline)
+
+
+class TimelineService(BaseService):
+    pass
