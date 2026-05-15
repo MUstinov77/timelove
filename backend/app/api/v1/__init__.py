d8f14426a48bd06876c140f8f17583e077781ad6
MUstinov77 <m.ustinov2001@yandex.ru>
2026-05-15 13:04:04 +0300

    include auth router

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from backend.app.api.v1 import timeline
-from backend.app.api.v1 import event
+from backend.app.api.v1 import auth, event, timeline
 
 api_router = APIRouter()
 
+api_router.include_router(auth.router)
 api_router.include_router(timeline.router)
 api_router.include_router(event.router)
 
