8cc0545c7b985a6c6ecd81d362e0bb228d0fcda2
MUstinov77 <m.ustinov2001@yandex.ru>
2026-05-15 12:26:51 +0300

    create user service

---
+++ backend/app/service/user.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .base import BaseService
+from backend.app.core.datastore import postgres_session_provider
+from backend.app.model.user import User
+
+
+def get_user_service(
+        session: AsyncSession = Depends(postgres_session_provider)
+):
+    return UserService(session, User)
+
+
+class UserService(BaseService):
+    pass
