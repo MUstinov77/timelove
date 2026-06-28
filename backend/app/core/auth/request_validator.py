b0b3d7b03d2af7dbc759c2057443bffd3bbfc8e6
MUstinov77 <m.ustinov2001@yandex.ru>
2026-06-28 17:28:11 +0300

    http exception for auth user dependency

--- backend/app/core/auth/request_validator.py
+++ backend/app/core/auth/request_validator.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 
+from jwt.exceptions import ExpiredSignatureError, DecodeError, InvalidTokenError
+
 from backend.app.core.auth.jwt import JWTService
-from backend.app.model.user import User
-from backend.app.service.user import UserService, get_user_service
-from fastapi import Depends
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
 auth_schema = OAuth2PasswordBearer(tokenUrl="auth/login")
@@ -11,8 +12,10 @@ auth_schema = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 async def authenticate_user(
         token: Annotated[str, Depends(auth_schema)],
-        user_service: Annotated[UserService, Depends(get_user_service)]
 ):
-    payload = JWTService().decode_token(token)
-    user_dict = payload.get("context")
-    return user_dict
+    try:
+        payload = JWTService().decode_token(token)
+        user_dict = payload.get("context")
+        return user_dict
+    except (ExpiredSignatureError, DecodeError, InvalidTokenError):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
