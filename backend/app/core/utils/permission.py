ce83546004cc998359d5cab8c9feee3cd7c49f6c
Loqi <notaloqi@MacBook-Air-Loqi.local>
2026-08-04 13:54:27 +0300

    change variable name

--- backend/app/core/utils/permission.py
+++ backend/app/core/utils/permission.py
@@ -8,7 +8,7 @@ from backend.app.service.member import MemberService, get_member_service
 
 
 def check_permission_dependency(
-        permission: MemberPermission
+        required_permission: MemberPermission
 ):
     async def dependency(
             timeline_id: int,
@@ -19,13 +19,13 @@ def check_permission_dependency(
         membership = await member_service.retrieve_membership(timeline_id, user_id)
         if not membership:
             raise PermissionException
-        return match_user_permission(membership.member_permission, permission)
+        return match_user_permission(membership.member_permission, required_permission)
 
     return dependency
 
 def match_user_permission(
         user_permission: MemberPermission,
-        necessary_permission: MemberPermission
+        required_permission: MemberPermission
 ):
     unknown_permission_type = -1
     permissions_order = {
@@ -33,10 +33,10 @@ def match_user_permission(
         MemberPermission.MODERATOR: 2,
         MemberPermission.ADMIN: 3,
     }
-    necessary_permission_value = permissions_order.get(necessary_permission, unknown_permission_type)
+    required_permission_value = permissions_order.get(required_permission, unknown_permission_type)
     user_permission_value = permissions_order.get(user_permission, unknown_permission_type)
-    if necessary_permission_value == unknown_permission_type or user_permission_value == unknown_permission_type:
+    if required_permission_value == unknown_permission_type or user_permission_value == unknown_permission_type:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unknown permission level")
-    if user_permission_value < necessary_permission_value:
+    if user_permission_value < required_permission_value:
         raise PermissionException
     return True
