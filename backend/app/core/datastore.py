8c46bf28ce4bf770343c3a7d37db21b3697fb5dd
Loqi <m.ustinov2001@yandex.ru>
2026-08-10 15:30:26 +0300

    return commit

--- backend/app/core/datastore.py
+++ backend/app/core/datastore.py
@@ -41,6 +41,7 @@ async def get_postgres_session():
     async with async_session_maker() as session:
         try:
             yield session
+            await session.commit()
         except Exception:
             await session.rollback()
             raise
