6b99cf9ba4874caf432777a405e60e4fa0337a80
Loqi <m.ustinov2001@yandex.ru>
2026-09-04 17:44:33 +0300

    uniq emails for users

--- backend/app/model/user.py
+++ backend/app/model/user.py
@@ -15,7 +15,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String())
+    email: Mapped[str] = mapped_column(String(), unique=True)
     hashed_password: Mapped[str] = mapped_column(String())
     first_name: Mapped[str] = mapped_column(String(), nullable=True)
     last_name: Mapped[str] = mapped_column(String(), nullable=True)
