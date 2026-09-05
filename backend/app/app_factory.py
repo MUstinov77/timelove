3aa3e5866c97ba489b02597eba98cfd55637ca29
Loqi <m.ustinov2001@yandex.ru>
2026-09-05 16:32:34 +0300

    change cors config

--- backend/app/app_factory.py
+++ backend/app/app_factory.py
@@ -40,7 +40,6 @@ def create_app():
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
